317ba50faffbc44cb5f296fc416814ec08e9d1a1
Pratyush2710 <pratyushhg@gmail.com>
2020-08-06 02:02:24 +0530

    navbar git added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import MarkDownLogo from "./markdown.svg";
+import GitHub from "./github.svg";
 function Navbar() {
   return (
     <nav className="navbar">
@@ -7,6 +8,12 @@ function Navbar() {
         <img src={MarkDownLogo} alt="" />
         &nbsp;Markdown Editor
       </h3>
+      <img
+        onClick={() => window.open("https://github.com/Pratyush2710")}
+        className="github"
+        src={GitHub}
+        alt=""
+      />
     </nav>
   );
 }
